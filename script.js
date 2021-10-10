2c180990530e8ef764282318e0b78e6c0dc81a51
SVillajos <s.villajos92@gmail.com>
2021-10-10 15:53:08 +0200

    arrays def.

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 // creation of the three arrays
-const ChristieArr = [    
-    [
-      "Father Burgundy",
-      "Constable Grey",
-      "Sir Orange",
-      "Master Purple",
-      "Duke Indigo",
-      "Principal Red",
-      "Grandfather Smith",
-      "Commissioner Gordon"
-    ], 
-    [
-      "the sauna",
-      "the larder",
-      "the study",
-      "the laundry room",
-      "the garden",
-      "the bar",
-      "the gallery",
-      "the pool",
-      "the cellar"
-    ], 
-    [
-      "bowling ball",
-      "revolver",
-      "rope",
-      "knife",
-      "pudgel",
-      "bazooka",
-      "lightsaber",
-      "shard of glass",
-      "stapler"
-    ]
+const personArr = 
+  [
+    "Father Burgundy",
+    "Constable Grey",
+    "Sir Orange",
+    "Master Purple",
+    "Duke Indigo",
+    "Principal Red",
+    "Grandfather Smith",
+    "Commissioner Gordon"
   ];
+
+const locationArr = 
+  [
+    "the sauna",
+    "the larder",
+    "the study",
+    "the laundry room",
+    "the garden",
+    "the bar",
+    "the gallery",
+    "the pool",
+    "the cellar"
+  ];
+  
+const toolArr = 
+  [
+    "bowling ball",
+    "revolver",
+    "rope",
+    "knife",
+    "pudgel",
+    "bazooka",
+    "lightsaber",
+    "shard of glass",
+    "stapler"
+  ]
+  
 
