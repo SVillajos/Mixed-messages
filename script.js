1630cab61d8179011437f4d1ea522468faf27b86
SVillajos <s.villajos92@gmail.com>
2021-10-10 14:42:35 +0200

    creation of the variables

---
+++ script.js
@@ -0,0 +1,35 @@
+// creation of the three arrays
+const ChristieArr = [    
+    [
+      "Father Burgundy",
+      "Constable Grey",
+      "Sir Orange",
+      "Master Purple",
+      "Duke Indigo",
+      "Principal Red",
+      "Grandfather Smith",
+      "Commissioner Gordon"
+    ], 
+    [
+      "the sauna",
+      "the larder",
+      "the study",
+      "the laundry room",
+      "the garden",
+      "the bar",
+      "the gallery",
+      "the pool",
+      "the cellar"
+    ], 
+    [
+      "bowling ball",
+      "revolver",
+      "rope",
+      "knife",
+      "pudgel",
+      "bazooka",
+      "lightsaber",
+      "shard of glass",
+      "stapler"
+    ]
+  ];
